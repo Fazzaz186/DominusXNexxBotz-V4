911a03650f433ef05d367b5b0aee42d5def450e0
Fazzaz186 <111657042+Fazzaz186@users.noreply.github.com>
2023-01-03 18:32:13 +0800

    Update info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 import fs from 'fs'
 let handler = async (m, { conn, args, command }) => {
-    const ultah = new Date('Februar 02 2023 00:00:01')
+    const ultah = new Date('january 15 2023 00:00:01')
     const sekarat = new Date().getTime() 
     const Kurang = ultah - sekarat
     const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
