8137a0f5d35b82c4911d2c0ffa473ee96a450ca1
Fazzaz186 <111657042+Fazzaz186@users.noreply.github.com>
2023-01-03 18:52:31 +0800

    Update owner-boton.js

--- plugins/owner-boton.js
+++ plugins/owner-boton.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
     global.db.data.chats[m.chat].isBanned = false
-    m.reply(' *Sukses Menghidupkan ShisuiBotz 🤖* ')
+    m.reply(' *Sukses Menghidupkan DominusBotz 🤖* ')
 }
 
 
